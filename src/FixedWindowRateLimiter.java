38c9271e333b0a3e29cd3b04681ab64f16782963
ihor <toolbox@tut.by>
2026-06-16 16:32:23 +0400

    FixedWindowRateLimiter

---
+++ src/FixedWindowRateLimiter.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FixedWindowRateLimiter {
+
+    private ConcurrentHashMap<String, AtomicInteger> map = new ConcurrentHashMap<>();
+    private int requestLimit = 5;
+    private long WINDOWS_FRAME = 500; //0,5 sec
+    private long windowsStart = System.currentTimeMillis();
+
+    synchronized boolean allowRequest(String userId) {
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime - windowsStart > WINDOWS_FRAME) {
+           windowsStart = currentTime;
+           map.clear();
+        }
+        map.putIfAbsent(userId, new AtomicInteger(0));
+        return map.get(userId).incrementAndGet() <= requestLimit;
+    }
+}
